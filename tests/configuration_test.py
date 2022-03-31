b9934a8bac5c7bfc384141f4797fdc9f4f1981aa
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2022-03-31 22:52:35 +0200

    add test for "init_config()"

--- tests/configuration_test.py
+++ tests/configuration_test.py
@@ -1,8 +1,12 @@
 # -*- coding: UTF-8 -*-
 
-from pythonic_testcase import *
+from configparser import ConfigParser
+from pathlib import Path
 
-from schwarz.filtering_proxy import Configuration, Domainlist
+from pythonic_testcase import *
+from schwarz.fakefs_helpers import FakeFS
+
+from schwarz.filtering_proxy import init_config, Configuration, Domainlist
 
 
 class ConfigurationTest(PythonicTestCase):
@@ -17,4 +21,42 @@ class ConfigurationTest(PythonicTestCase):
         assert_true(cfg.is_allowed('baz.example'))
         cfg.config['default_rule'] = 'block'
         assert_false(cfg.is_allowed('baz.example'))
+
+    def test_can_init_config_from_file(self):
+        self.fs = FakeFS.set_up(test=self)
+        base_path = Path(self.fs.create_dir('data').path)
+        create_cfg_dirs(base_path)
+        config_path = base_path/'cfg'/'proxy.cfg'
+        create_config(config_path, rule_basedir=base_path, default_rule='block')
+        cfg = init_config(config_path)
+
+        create_rule(domain='foo.example', allow=True, rule_basedir=base_path)
+        assert_true(cfg.is_allowed('foo.example'))
+        assert_false(cfg.is_allowed('bar.example'))
+
+
+
+def create_cfg_dirs(rule_basedir):
+    (rule_basedir/'allowed.d').mkdir()
+    (rule_basedir/'blocked.d').mkdir()
+
+def create_config(config_path, rule_basedir, *, default_rule=None):
+    config = ConfigParser()
+    config['proxy'] = {}
+    cfg_section = config['proxy']
+    cfg_section['rule_basedir'] = str(rule_basedir)
+    if default_rule:
+        cfg_section['default_rule'] = default_rule
+    config_dir = config_path.parent
+    config_dir.mkdir(exist_ok=True)
+    with config_path.open('x') as config_fp:
+        config.write(config_fp)
+
+def create_rule(domain, *, allow=True, rule_basedir):
+    dirname = 'allowed.d' if allow else 'blocked.d'
+    path_rulesdir = rule_basedir / dirname
+    path_rulesdir.mkdir(exist_ok=True)
+    rulefile_path = path_rulesdir / f'{domain}.domains'
+    with rulefile_path.open('x') as fp:
+        fp.write(f'{domain}\n')
 
